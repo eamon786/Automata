014d4a374c4b20337efd90893f8f3b0192b02556
eamon786 <202433920+eamon786@users.noreply.github.com>
2026-02-02 13:12:45 +0100

    aggiunto il metodo per gestire regola

--- Automa1D.java
+++ Automa1D.java
@@ -1,29 +1,43 @@
 class Automa1D {
 
     private int[] striscia;
-    private int regola;
+    private static int regola;
+    
+        public Automa1D(int larghezza, int regola) {
+            this.striscia = new int[larghezza];
+            this.regola = regola;
+            striscia[larghezza / 2] = 1; // solo una cella accesa al centro. Ci serve per
+            // vedere se si "propaga"
+        }
+    
+        public static int calcolaStato(int sinistra, int centro, int destra) {
+            regola = 110;
 
-    public Automa1D(int larghezza, int regola) {
-        this.striscia = new int[larghezza];
-        this.regola = regola;
-        striscia[larghezza / 2] = 1; // solo una cella accesa al centro. Ci serve per
-        // vedere se si "propaga"
-    }
+            String binario = Integer.toBinaryString(regola); 
+            char[] n = new char[binario.length()];
+            for (int i = 0; i < n.length; i++) {
+                n[i] = binario.charAt(i);    
+                System.out.println(n[i]);
+            }
 
-    public int calcolaStato(int sinistra, int centro, int destra) {
-        // TODO Completa qui: in base a sinistra, centro, destra e la "regola"
-        // devi calcolare il nuovo stato
-    }
-
-    public void nextStato() {
-        int[] nuovoStato = new int[striscia.length];
-
-        // TODO Completa qui:
-        // per ogni cella, prendi i vicini e calcola il nuovo stato usando
-        // "calcolaStato"
-        striscia = nuovoStato;
-    }
-    public static void main(String[] args) {
-        System.out.println("hello");
+            System.out.println("numero in binario: " + binario);
+            
+            // TODO Completa qui: in base a sinistra, centro, destra e la "regola"
+            
+            // devi calcolare il nuovo stato
+            return 0;
+        }
+    
+        public void nextStato() {
+            int[] nuovoStato = new int[striscia.length];
+    
+            // TODO Completa qui:
+            // per ogni cella, prendi i vicini e calcola il nuovo stato usando
+            // "calcolaStato"
+             striscia = nuovoStato;
+        }
+        public static void main(String[] args) {
+            System.out.println("hello");
+            calcolaStato(0,0,0);
     }
 }
