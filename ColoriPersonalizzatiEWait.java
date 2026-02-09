534b7755ff2c9e3e1e9efe69e6582d6d2fe6649b
eamon786 <202433920+eamon786@users.noreply.github.com>
2026-02-09 12:36:06 +0100

    Fix nextStato -nuovoStato[i] = int cal

--- ColoriPersonalizzatiEWait.java
+++ ColoriPersonalizzatiEWait.java
@@ -75,8 +75,8 @@ public class ColoriPersonalizzatiEWait {
     }
 
     public static void main(String[] args) throws InterruptedException {
-         provaColori();
+        // provaColori();
 
-        //barraCaricamento();
+        barraCaricamento();
     }
 }
